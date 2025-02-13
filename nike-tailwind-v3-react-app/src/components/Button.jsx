e47d05e8808b809c6c5436ad906358485b2e3459
Nithin Mathew <nithin.mathew@sysfore.com>
2025-02-13 16:05:25 +0530

    super quality section implemented

--- nike-tailwind-v3-react-app/src/components/Button.jsx
+++ nike-tailwind-v3-react-app/src/components/Button.jsx
@@ -5,7 +5,13 @@ const Button = ({ label, iconUrl }) => {
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
 
-      <img src={iconUrl} alt="Icon URL" className="ml-2 rounded-full w-5 h-5" />
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="Icon URL"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
