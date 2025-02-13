b2071b28c76209764f8636b7844df47cb4aca1e4
Nithin Mathew <nithin.mathew@sysfore.com>
2025-02-13 17:32:20 +0530

    special offer implemented

--- nike-tailwind-v3-react-app/src/components/Button.jsx
+++ nike-tailwind-v3-react-app/src/components/Button.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+
+  fullWidth,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : "bg-coral-red text-white border-coral-red"
+    } rounded-full ${fullWidth && "w-full"}`}
+    >
       {label}
 
       {iconUrl && (
