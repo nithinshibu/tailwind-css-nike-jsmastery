170d5acda3bee500682c9849a397cabcbe00da4e
Nithin Mathew <nithin.mathew@sysfore.com>
2025-02-13 17:08:44 +0530

    service section implemented

--- nike-tailwind-v3-react-app/src/sections/Services.jsx
+++ nike-tailwind-v3-react-app/src/sections/Services.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service}></ServiceCard>
+      ))}
+    </section>
+  );
 };
 
 export default Services;
